e85c23d67073121f14697e0dc1b4497b6d234602
L. Pommeret <135142064+l-pommeret@users.noreply.github.com>
2026-08-11 16:22:12 +0200

    Reconcile Star11 summary and proof loci

--- Principia/FirstEdition/Volume1/Star11.lean
+++ Principia/FirstEdition/Volume1/Star11.lean
@@ -17,24 +17,35 @@ PM-VERBATIM-END PM1:✱11·05 -/
 /- PM-VERBATIM-BEGIN PM1:✱11·06
 ✱11·06.  φ(x, y) .≡ₓ,ᵧ. ψ(x, y) .= : (x, y) : φ(x, y) .≡ .ψ(x, y)  Df
 PM-VERBATIM-END PM1:✱11·06 -/
-/- PM-VERBATIM-BEGIN PM1:✱11·1
+/- PM-VERBATIM-SUMMARY-BEGIN PM1:✱11·1
 ✱11·1.  ⊢ : (x, y).φ(x, y) .⊃ .φ(z, w)
-PM-VERBATIM-END PM1:✱11·1 -/
-/- PM-VERBATIM-BEGIN PM1:✱11·11
+PM-VERBATIM-SUMMARY-END PM1:✱11·1 -/
+/- PM-VERBATIM-SUMMARY-BEGIN PM1:✱11·11
 ✱11·11.  If φ(z, w) is true whatever possible arguments z and w may be,
 then (x, y).φ(x, y) is true.
-PM-VERBATIM-END PM1:✱11·11 -/
-/- PM-VERBATIM-BEGIN PM1:✱11·2
+PM-VERBATIM-SUMMARY-END PM1:✱11·11 -/
+/- PM-VERBATIM-SUMMARY-BEGIN PM1:✱11·2
 ✱11·2.  ⊢ : (x, y).φ(x, y) .≡ .(y, x).φ(x, y)
-PM-VERBATIM-END PM1:✱11·2 -/
-/- PM-VERBATIM-BEGIN PM1:✱11·3
+PM-VERBATIM-SUMMARY-END PM1:✱11·2 -/
+/- PM-VERBATIM-SUMMARY-BEGIN PM1:✱11·3
 ✱11·3.  ⊢ : .p .⊃ .(x, y).φ(x, y) .≡ : (x, y) : p .⊃ .φ(x, y)
-PM-VERBATIM-END PM1:✱11·3 -/
+PM-VERBATIM-SUMMARY-END PM1:✱11·3 -/
 /- PM-VERBATIM-BEGIN PM1:✱11·07
 ✱11·07.  “Whatever possible argument x may be, φ(x, y) is true whatever
 possible argument y may be” implies the corresponding statement with x and y
 interchanged.  Pp.
 PM-VERBATIM-END PM1:✱11·07 -/
+/- PM-VERBATIM-BEGIN PM1:✱11·1
+✱11·1.  ⊢ : (x, y).φ(x, y) .⊃ .φ(z, w)
+Dem.  ⊢ . ✱10·1 . ⊃ ⊢ : Hp .⊃ .(y).φ(z, y).
+[✱10·1] ⊃ .φ(z, w) .⊃ ⊢ . Prop.
+PM-VERBATIM-END PM1:✱11·1 -/
+/- PM-VERBATIM-BEGIN PM1:✱11·11
+✱11·11.  If φ(z, w) is true whatever possible arguments z and w may be,
+then (x, y).φ(x, y) is true.
+Dem.  By ✱10·11, the hypothesis implies that (y).φ(z, y) is true whatever
+possible argument z may be; and this, by ✱10·11, implies (x, y).φ(x, y).
+PM-VERBATIM-END PM1:✱11·11 -/
 /- PM-VERBATIM-BEGIN PM1:✱11·12
 ✱11·12.  ⊢ : .(x, y).p∨φ(x, y) .⊃ : p .∨ .(x, y).φ(x, y)
 Dem.  By ✱10·12, ✱10·11·27, and ✱10·12, ⊢ . Prop.
