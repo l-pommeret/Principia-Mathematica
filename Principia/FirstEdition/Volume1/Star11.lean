e75e92d366ab470e1109a258ee890f545618dcff
L. Pommeret <135142064+l-pommeret@users.noreply.github.com>
2026-08-11 16:12:25 +0200

    Collate source backfill for Q276

--- Principia/FirstEdition/Volume1/Star11.lean
+++ Principia/FirstEdition/Volume1/Star11.lean
@@ -14,3 +14,19 @@ PM-VERBATIM-END PM1:✱11·04 -/
 /- PM-VERBATIM-BEGIN PM1:✱11·05
 ✱11·05.  φ(x, y) .⊃ₓ,ᵧ. ψ(x, y) .= : (x, y) : φ(x, y) .⊃ .ψ(x, y)  Df
 PM-VERBATIM-END PM1:✱11·05 -/
+/- PM-VERBATIM-BEGIN PM1:✱11·06
+✱11·06.  φ(x, y) .≡ₓ,ᵧ. ψ(x, y) .= : (x, y) : φ(x, y) .≡ .ψ(x, y)  Df
+PM-VERBATIM-END PM1:✱11·06 -/
+/- PM-VERBATIM-BEGIN PM1:✱11·1
+✱11·1.  ⊢ : (x, y).φ(x, y) .⊃ .φ(z, w)
+PM-VERBATIM-END PM1:✱11·1 -/
+/- PM-VERBATIM-BEGIN PM1:✱11·11
+✱11·11.  If φ(z, w) is true whatever possible arguments z and w may be,
+then (x, y).φ(x, y) is true.
+PM-VERBATIM-END PM1:✱11·11 -/
+/- PM-VERBATIM-BEGIN PM1:✱11·2
+✱11·2.  ⊢ : (x, y).φ(x, y) .≡ .(y, x).φ(x, y)
+PM-VERBATIM-END PM1:✱11·2 -/
+/- PM-VERBATIM-BEGIN PM1:✱11·3
+✱11·3.  ⊢ : .p .⊃ .(x, y).φ(x, y) .≡ : (x, y) : p .⊃ .φ(x, y)
+PM-VERBATIM-END PM1:✱11·3 -/
