0be878615b87f90a7c10f58df0028f3a70301b66
L. Pommeret <135142064+l-pommeret@users.noreply.github.com>
2026-08-11 15:47:23 +0200

    Collate source backfill for Q265

--- Principia/FirstEdition/Volume1/Star10.lean
+++ Principia/FirstEdition/Volume1/Star10.lean
@@ -14,3 +14,18 @@ PM-VERBATIM-END PM1:✱10·1 -/
 /- PM-VERBATIM-BEGIN PM1:✱10·11
 ✱10·11.  If φy is true whatever possible argument y may be, then (x).φx is true.
 PM-VERBATIM-END PM1:✱10·11 -/
+/- PM-VERBATIM-BEGIN PM1:✱10·27
+✱10·27.  ⊢ : .(z).φz ⊃ ψz .⊃ : (z).φz .⊃ .(z).ψz
+PM-VERBATIM-END PM1:✱10·27 -/
+/- PM-VERBATIM-BEGIN PM1:✱10·271
+✱10·271.  ⊢ : .(z).φz ≡ ψz .⊃ : (z).φz .≡.(z).ψz
+PM-VERBATIM-END PM1:✱10·271 -/
+/- PM-VERBATIM-BEGIN PM1:✱10·28
+✱10·28.  ⊢ : .(x).φx ⊃ ψx .⊃ : (∃x).φx .⊃ .(∃x).ψx
+PM-VERBATIM-END PM1:✱10·28 -/
+/- PM-VERBATIM-BEGIN PM1:✱10·281
+✱10·281.  ⊢ : .(x).φx ≡ ψx .⊃ : (∃x).φx .≡.(∃x).ψx
+PM-VERBATIM-END PM1:✱10·281 -/
+/- PM-VERBATIM-BEGIN PM1:✱10·35
+✱10·35.  ⊢ : .(∃x).p .φx .≡ : p : (∃x).φx
+PM-VERBATIM-END PM1:✱10·35 -/
