beec1dc0d058d65b131dada43cbe0a580f1b69a4
L. Pommeret <135142064+l-pommeret@users.noreply.github.com>
2026-08-12 03:14:50 +0200

    Catalog PM III star 255 Q677

---
+++ Principia/FirstEdition/Volume3/Star255P64cSource.lean
@@ -0,0 +1,9 @@
+/- PM-VERBATIM-BEGIN PM3:✱255·27
+✱255·27. ⊢ : NrʻP < NrʻQ .≡ . NrʻP ≤ NrʻQ . NrʻP ≠ NrʻQ
+PM-VERBATIM-END PM3:✱255·27 -/
+/- PM-VERBATIM-BEGIN PM3:✱255·28
+✱255·28. ⊢ : NrʻP > NrʻQ .≡ . NrʻP ≥ NrʻQ . ∼(NrʻQ ≥ NrʻP) .≡ . P, Q ∈ Ω . ∼(NrʻQ ≥ NrʻP)  [✱255·13·22·21]
+PM-VERBATIM-END PM3:✱255·28 -/
+/- PM-VERBATIM-BEGIN PM3:✱255·281
+✱255·281. ⊢ : μ > ν .≡ . μ ≥ ν . ∼(ν ≥ μ) .≡ . μ, ν ∈ N₀O . ∼(ν ≥ μ)  [✱255·114]
+PM-VERBATIM-END PM3:✱255·281 -/
